421b2517f8490818c479900397bf63c94703a4ed
Gregor Ihmor <gregor.ihmor@gmail.com>
2021-01-27 09:51:31 +0100

    improved non-square number inputs

--- frontend/src/main/scala/frontend/components/InputNumberSVG.scala
+++ frontend/src/main/scala/frontend/components/InputNumberSVG.scala
@@ -10,48 +10,47 @@ object InputNumberSVG {
   val fontSize = 0.8.toString
 
   def apply(dim: Dimensions, interaction: Option[Interaction]): Node = {
-    val cell = Math.ceil(Math.sqrt(dim.blockSize)).toInt
     Node("svg.number-input")
       .attr("xmlns", "http://www.w3.org/2000/svg")
-      .attr("viewBox", s"0 0 $cell ${cell + 1}")
-      .childOptional(interaction.map(interactionRects(dim, cell, _)))
-      .children(grid(cell), values(dim, cell))
+      .attr("viewBox", s"0 0 ${dim.width} ${dim.height + 1}")
+      .childOptional(interaction.map(interactionRects(dim, _)))
+      .children(grid(dim), values(dim))
   }
 
-  private def grid(cell: Int): Node =
+  private def grid(dim: Dimensions): Node =
     Node("g")
       .attr("id", "grid")
       .style("pointer-events", "none")
       .child {
-        for (column <- 0 to cell)
+        for (column <- 0 to dim.width)
           yield Node("line")
             .attr("x1", column.toString)
             .attr("x2", column.toString)
             .attr("y1", "0")
-            .attr("y2", (if (column == 0 || column == cell) cell + 1 else cell).toString)
+            .attr("y2", (if (column == 0 || column == dim.width) dim.height + 1 else dim.height).toString)
             .attr("stroke", "black")
             .attr("stroke-width", scale)
       }
       .child {
-        for (row <- 0 to cell)
+        for (row <- 0 to dim.height)
           yield Node("line")
             .attr("x1", "0")
-            .attr("x2", cell.toString)
+            .attr("x2", dim.width.toString)
             .attr("y1", row.toString)
             .attr("y2", row.toString)
             .attr("stroke", "black")
             .attr("stroke-width", scale)
       }
 
-  private def values(dim: Dimensions, cell: Int): Node =
+  private def values(dim: Dimensions): Node =
     Node("g")
       .attr("id", "values")
       .style("pointer-events", "none")
       .child {
         for {
-          x <- 0 until cell
-          y <- 0 until cell
-          value = x + y * cell
+          x <- 0 until dim.width
+          y <- 0 until dim.height
+          value = x + y * dim.width
           if value < dim.blockSize
         } yield Node("text")
           .attr("transform", s"translate($x $y)")
@@ -68,8 +67,8 @@ object InputNumberSVG {
       }
       .child(
         Node("image")
-          .attr("x", (cell / 2.0 - 0.5).toString)
-          .attr("y", cell.toString)
+          .attr("x", (dim.width / 2.0 - 0.5).toString)
+          .attr("y", dim.height.toString)
           .attr("width", "1")
           .attr("height", "1")
           .attr("href", "/trash.svg")
@@ -78,15 +77,14 @@ object InputNumberSVG {
           .style("transform-origin", "center")
       )
 
-  private def interactionRects(dim: Dimensions, cell: Int, interaction: Interaction): Node =
+  private def interactionRects(dim: Dimensions, interaction: Interaction): Node =
     Node("g")
       .attr("id", "interationRects")
       .child {
         for {
-          x <- 0 until cell
-          y <- 0 until cell
-          value = x + y * cell
-          if value < dim.blockSize
+          x <- 0 until dim.width
+          y <- 0 until dim.height
+          value = x + y * dim.width
         } yield interaction(
           Some(value + 1),
           Node("rect")
@@ -103,8 +101,8 @@ object InputNumberSVG {
           None,
           Node("rect")
             .attr("x", "0")
-            .attr("y", cell.toString)
-            .attr("width", cell.toString)
+            .attr("y", dim.height.toString)
+            .attr("width", dim.width.toString)
             .attr("height", "1")
             .attr("fill", "rgba(0, 0, 0, 0)")
             .attr("stroke", "none")
