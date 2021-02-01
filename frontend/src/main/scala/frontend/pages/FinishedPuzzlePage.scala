1b7a66197fb407814311a33d6e09bf6fb86e038b
Gregor Ihmor <gregor.ihmor@gmail.com>
2021-02-01 21:55:59 +0100

    reworked animation ... again

--- frontend/src/main/scala/frontend/pages/FinishedPuzzlePage.scala
+++ frontend/src/main/scala/frontend/pages/FinishedPuzzlePage.scala
@@ -15,7 +15,8 @@ import scala.util.Random
 @Lenses
 case class FinishedPuzzleState(
     board: DecoratedBoard,
-    tapped: Boolean = false
+    tapped: Boolean = false,
+    bloom: Position
 ) extends PageState {
   require(board.data.forall {
     case Given(_) | Input(_) => true
@@ -23,7 +24,36 @@ case class FinishedPuzzleState(
   })
 }
 
+object FinishedPuzzleState {
+  def apply(board: DecoratedBoard): FinishedPuzzleState =
+    FinishedPuzzleState(board, bloom = randomPosition(board.dim))
+
+  def randomPosition(dim: Dimensions): Position = {
+    val positions = SudokuBoard.positions(dim)
+    positions((Math.random() * positions.length).toInt)
+  }
+}
+
 object FinishedPuzzlePage extends Page[FinishedPuzzleState] with NoRouting {
+  private object Animation {
+    val speed = 0.15
+    val duration = 0.8
+    def pause(dim: Dimensions) = speed * (dim.blockSize - 1) * Math.sqrt(2d)
+    def interval(dim: Dimensions): Double = duration + pause(dim)
+
+    def bloomAnimation(bloom: Position)(pos: Position, node: Node): Node = {
+      def distance(p: Position, q: Position): Double = {
+        val dx = p._1 - q._1
+        val dy = p._2 - q._2
+        Math.sqrt(dx * dx + dy * dy)
+      }
+
+      node.style(
+        "animation",
+        s"finished-animation ${Animation.duration}s linear ${distance(bloom, pos) * Animation.speed}s 1 both"
+      )
+    }
+  }
 
   override def render(implicit context: Context): Node =
     Node("div.grid-layout")
@@ -34,13 +64,28 @@ object FinishedPuzzlePage extends Page[FinishedPuzzleState] with NoRouting {
           .child(
             SudokuBoardSVG(
               board = context.local.board,
-              interaction = Some(animations(context.local.board))
+              interaction = Some(Animation.bloomAnimation(context.local.bloom))
             ).classes("grid-main-svg", "finished-sudoku")
               .event("click", Action(FinishedPuzzleState.tapped.set(true)))
+              .key(Math.random)
           )
       )
       .child(buttonBar().classes("grid-footer", "buttons", "my-2"))
       .maybeModify(context.local.tapped)(_.child(finishedModal()))
+      .hookInsert { _ =>
+        setTimeout()
+      }
+      .hookPostpatch { (_, _) =>
+        setTimeout()
+      }
+
+  private def setTimeout()(implicit context: Context): Unit =
+    dom.window.setTimeout(
+      () =>
+        context.update(context.local.copy(bloom = FinishedPuzzleState.randomPosition(context.local.board.dim)))
+      ,
+      Animation.interval(context.local.board.dim) * 1000
+    )
 
   private def finishedModal()(implicit context: Context): Node =
     Modal(closeAction = Some(Action(FinishedPuzzleState.tapped.set(false))))(
@@ -53,49 +98,4 @@ object FinishedPuzzlePage extends Page[FinishedPuzzleState] with NoRouting {
         .classes("is-primary")
     )
 
-  // note: there is an experimental API which would simplify this quite a lot:
-  // https://developer.mozilla.org/en-US/docs/Web/API/Animation/Animation
-  private def animations(sudokuBoard: SudokuBoard[_])(pos: Position, node: Node): Node = {
-    def distance(p: Position, q: Position): Double = {
-      val dx = p._1 - q._1
-      val dy = p._2 - q._2
-      Math.sqrt(dx * dx + dy * dy)
-    }
-
-    val dim = sudokuBoard.dim
-
-    val random = new Random(sudokuBoard.hashCode())
-    val bloomOrigins = Iterator.continually {
-      val i = random.nextInt(dim.boardSize)
-      SudokuBoard.positions(dim)(i)
-    }
-
-    val speed    = 0.15
-    val duration = 0.8
-    val pause    = speed * distance((0, 0), (dim.blockSize - 1, sudokuBoard.dim.blockSize - 1))
-
-    var tick = 0
-
-    def animation() =
-      s"finished-animation ${duration}s linear ${distance(bloomOrigins.next(), pos) * speed + (duration + pause) * tick}s 1 both"
-
-    var intervalHandle: Option[Int] = None
-
-    node
-      .hookInsert { elem =>
-        elem.elm.get.style.animation = animation()
-        intervalHandle = Some(
-          dom.window.setInterval(
-            () => {
-              tick = tick + 1
-              elem.elm.get.style.animation = animation()
-            },
-            (duration + pause) * 1000
-          )
-        )
-      }
-      .hookDestroy { _ =>
-        intervalHandle.foreach(dom.window.clearInterval)
-      }
-  }
 }
