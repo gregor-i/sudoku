9ede9163011080aa39c4c16275294cfaf9718577
Gregor Ihmor <gregor.ihmor@gmail.com>
2021-10-26 12:30:04 +0200

    bugfix

--- model/src/main/scala/model/infinite/ContinuePuzzle.scala
+++ model/src/main/scala/model/infinite/ContinuePuzzle.scala
@@ -8,52 +8,38 @@ import scala.util.chaining.*
 
 object ContinuePuzzle {
   def maybeContinue(puzzle: SudokuPuzzle, seed: Int, difficulty: Difficulty): Option[SudokuPuzzle] = {
-    def subsetIsCompleted(subset: Subset): Boolean = subset.forall(pos => puzzle.get(pos).isCorrectAndFilled)
-
-    val completedRowsAndColumns = SudokuBoard
+    val positions = SudokuBoard
       .columnBlocks(puzzle.dim)
       .concat(SudokuBoard.rowBlocks(puzzle.dim))
-      .filter(subsetIsCompleted)
+      .filter(_.forall(pos => puzzle.get(pos).isCorrectAndFilled))
       .flatten
       .distinct
 
-    if (completedRowsAndColumns.nonEmpty)
-      Some(apply(puzzle, completedRowsAndColumns, seed, difficulty))
-    else
-      None
-  }
+    if (positions.nonEmpty) {
+      val random = Random(seed)
 
-  def apply(
-      puzzle: SudokuPuzzle,
-      positions: Seq[Position],
-      seed: Int,
-      difficulty: Difficulty
-  ): SudokuPuzzle = {
-    require(puzzle.data.forall(_.isCorrectAndFilled))
+      val alternativeSolution = ContinuationOptions(puzzle.map(_.solution), positions.toSet, seed = random.nextInt()).head
 
-    val random = Random(seed)
-
-    val alternativeSolution = ContinuationOptions(puzzle.map(_.solution), positions.toSet, seed = random.nextInt()).head
-
-    val inputForGenerator =
-      merge(
-        puzzle.map[PuzzleCell.Given | PuzzleCell.Empty] {
-          case cell: PuzzleCell.Given => cell
-          case cell                   => PuzzleCell.Empty(cell.solution)
-        },
-        alternativeSolution.map(PuzzleCell.Given.apply),
-        positions
-      )
-
-    val continuedPuzzle =
-      Generator
-        .makePuzzle(
-          positions = random.shuffle(positions),
-          board = inputForGenerator,
-          solver = Solver.forDifficulty(difficulty)
+      val inputForGenerator =
+        merge(
+          puzzle.map[PuzzleCell.Given | PuzzleCell.Empty] {
+            case cell: PuzzleCell.Given => cell
+            case cell                   => PuzzleCell.Empty(cell.solution)
+          },
+          alternativeSolution.map(PuzzleCell.Given.apply),
+          positions
         )
 
-    merge(puzzle, continuedPuzzle, positions)
+      val continuedPuzzle =
+        Generator
+          .makePuzzle(
+            positions = random.shuffle(positions),
+            board = inputForGenerator,
+            solver = Solver.forDifficulty(difficulty)
+          )
+
+      Some(merge(puzzle, continuedPuzzle, positions))
+    } else None
   }
 
   private def merge[A, B](left: SudokuBoard[A], right: SudokuBoard[B], positions: Seq[Position]): SudokuBoard[A | B] = {
